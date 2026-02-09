bfdcfa3a4d0fc7dca6c83498b198de254a65e7db
Jorge O. Castro <jorge.castro@gmail.com>
2026-02-08 23:15:17 -0500

    fix: remove blank line between url and name stanzas for style compliance

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -3,7 +3,6 @@ cask "sublime-text" do
   sha256 "36f69c551ad18ee46002be4d9c523fe545d93b67fea67beea731e724044b469f"
 
   url "https://download.sublimetext.com/sublime_text_build_#{version}_x64.tar.xz"
-
   name "Sublime Text"
   desc "Sophisticated text editor for code, markup and prose"
   homepage "https://www.sublimetext.com/"
